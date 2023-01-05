cf8a1f58968496df7480cc3d33ca44aabfef6d0c
Sylsee <sylvio.poliart@gmail.com>
2023-01-05 15:50:05 +0100

    blblbl

---
+++ srcs/requirements/wordpress/conf/install-wp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+# Download WordPress and extract it
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz -C /var/www/html/
+rm latest.tar.gz
+chown -R wordpress:wordpress /var/www/html/wordpress
+
+# Generate a WordPress configuration file
+cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/localhost/$DB_HOST/g" /var/www/html/wordpress/wp-config.php
